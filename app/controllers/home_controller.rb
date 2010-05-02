24276008d498f2ae7316c1b193a499e0b17025f8
David Ackerman <david.s.ackerman@gmail.com>
2010-05-02 17:22:25 -0600

    Basic photo upload to photo sheet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
 	layout "standard"
 	
 	def index
+		@image = params[:image]
+	end
 	
+	def upload
+		post = DataFile.save(params[:upload])
+		redirect_to :controller => "home", :action => "index", :image => File.basename(params[:upload]['datafile'].original_filename)
 	end
 end
